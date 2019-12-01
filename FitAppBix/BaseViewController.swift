190abd9c2e6a5e6c915098dd66a9cf3654de5944
PavelETU <pavelsuvit@gmail.com>
2019-12-01 23:45:23 +0000

    Refactor UI: open second screen by IBAction, add constraints, add stack views

--- FitAppBix/BaseViewController.swift
+++ FitAppBix/BaseViewController.swift
@@ -7,14 +7,16 @@
 //
 
 import UIKit
-
 class BaseViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func openBodyFatMeasurementsScreen(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "BodyFatMeasurementsScreen")
+        self.present(vc, animated: true, completion: nil)
+    }
+    
 }
 
